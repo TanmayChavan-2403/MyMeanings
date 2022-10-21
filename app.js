85f9f72d64ceee673a6ac9e4a6a07e6fa526408d
TanmayChavan-2403 <tanmaychavan1306@gmail.com>
2022-10-21 12:55:44 +0530

    Update schedule code

--- app.js
+++ app.js
@@ -9,18 +9,22 @@ const express = require('express');
 const path = require('path');
 
 // Global variables
-let DATE = "2022-10-20";
 var datePattern = /\d{4}-\d{2}-\d{2}/;
 var timePattern = /\d{1,2}:\d{1,2}:\d{1,2}/;
-var schedule1 = moment.tz(`${DATE} 12:00`, "Asia/Kolkata").format();
-var schedule2 = moment.tz(`${DATE} 17:30`, "Asia/Kolkata").format();
-var schedule3 = moment.tz(`${DATE} 21:30`, "Asia/Kolkata").format();
+var dateTime = moment.tz("Asia/Kolkata").format();
 const subscription = JSON.parse(process.env.ANDROID_SUBCRIPTION_URL);
 
 // Initializing firebase
 admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(process.env.SERVER_KEY))
 })
+
+// Applying settings of web-push
+webpush.setVapidDetails(
+    "mailto:tanmaychavan1306@gmail.com",
+    process.env.PUBLIC_KEY,
+    process.env.PRIVATE_KEY
+)
 
 // Initializing instance of firestore and express app
 const db = admin.firestore();
@@ -29,6 +33,8 @@ const app = express();
 // Parsing incoming requests with json payloads
 app.use(express.json());
 
+
+// Routes to handle incoming requests
 app.get('/sendLogFile', (req, res) => {
     console.log('Reached here...');
     res.sendFile(path.resolve(__dirname, "./log.txt"))
@@ -40,40 +46,28 @@ app.listen(process.env.PORT, () => {
 
 
 
-// Applying settings of web-push
-webpush.setVapidDetails(
-    "mailto:tanmaychavan1306@gmail.com",
-    process.env.PUBLIC_KEY,
-    process.env.PRIVATE_KEY
-)
-
 // Schedule operation for morning(12:00) notification
-schedule.scheduleJob(schedule1, () => {
-    let payload = JSON.stringify({title: `My Meanings for-${schedule1}`})
+schedule.scheduleJob("0 9 * * *", () => {
+    let payload = JSON.stringify({title: `My Meanings for-${dateTime}`})
     webpush.sendNotification(subscription, payload)
-    .then(res => methods.log(`Notification sent on ${schedule1}`))
+    .then(res => methods.log(`Notification sent on ${dateTime.match(datePattern)}`))
     .catch(err => methods.log(err))
 })
 
 // Schedule operation for evening(5:30) notification
-schedule.scheduleJob(schedule2, () => {
-    let payload = JSON.stringify({title: `1st Notification successfull!-${schedule2.match(timePattern)}`})
+schedule.scheduleJob("0 13 * * *", () => {
+    let payload = JSON.stringify({title: `1st Notification successfull!-${dateTime.match(timePattern)}`})
     webpush.sendNotification(subscription, payload)
-    .then(res => methods.log(`Notification sent on ${schedule2}`))
+    .then(res => methods.log(`Notification sent on 01:00 ${dateTime.match(datePattern)}`))
     .catch(err => methods.log(err))
 })
 
 // Schedule operation for night(9:30) notification
-schedule.scheduleJob(schedule3, () => {
-    let payload = JSON.stringify({title: `My Meanings for-${schedule3.match(timePattern)}`})
+schedule.scheduleJob("0 16 * * *", () => {
+    let payload = JSON.stringify({title: `My Meanings for-${dateTime.match(timePattern)}`})
     webpush.sendNotification(subscription, payload)
-    .then(res => methods.log(`Notification sent on ${schedule3.match(timePattern)}`))
+    .then(res => methods.log(`Notification sent on ${dateTime.match(timePattern)}`))
     .catch(err => methods.log(err))
-})
-
-// Updating the date variable at 12:01 each night
-schedule.scheduleJob("1 0 * * *", () => {
-    DATE = moment.tz('Asia/Kolkata').format().match(datePattern)[0];
 })
 
 // Logging and sending notification of first set-up time to console and in file.
